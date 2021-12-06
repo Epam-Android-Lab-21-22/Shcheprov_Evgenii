77ab838d9898af7d17b80e2bd4865a8e9410fe4a
ggendrick <evgenyscheprov@gmail.com>
2021-12-06 10:47:00 +0300

    resources fixed

--- app/src/main/java/com/example/shcheprov_homeworks/MainActivity.kt
+++ app/src/main/java/com/example/shcheprov_homeworks/MainActivity.kt
@@ -57,11 +57,9 @@ class MainActivity : AppCompatActivity() {
         }
         binding.rotationEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
             override fun onTextChanged(update: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 mainViewModel.updateCharacters(update.toString())
             }
-
             override fun afterTextChanged(p0: Editable?) {}
 
         })
